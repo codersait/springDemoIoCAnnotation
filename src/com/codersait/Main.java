8f2825f2d8076860900e07313e38c158bba387ff
saitg <codersait@gmail.com>
2020-12-01 14:25:12 +0300

    class config xml yerine

--- src/com/codersait/Main.java
+++ src/com/codersait/Main.java
@@ -1,11 +1,11 @@
 package com.codersait;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(IocConfig.class);
         ICustomerDao customerService = context.getBean("database", ICustomerDao.class);
         customerService.add();
 
