8b7cc7019c60171cf0a745604e92012d87e32358
saitg <codersait@gmail.com>
2020-12-01 14:59:11 +0300

    class config dosyasindan property injection

--- src/com/codersait/IocConfig.java
+++ src/com/codersait/IocConfig.java
@@ -3,9 +3,11 @@ package com.codersait;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.codersait")
+@PropertySource("values.properties")
 public class IocConfig {
     @Bean
     public ICustomerDao database(){
@@ -15,4 +17,5 @@ public class IocConfig {
     public ICustomerService service(){
         return new CustomerManager(database());
     }
+
 }
--- src/com/codersait/MySqlCustomerDao.java
+++ src/com/codersait/MySqlCustomerDao.java
@@ -1,6 +1,9 @@
 package com.codersait;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class MySqlCustomerDao implements ICustomerDao {
+    @Value("${database.ConnectionString}")
     private String connectionString;
 
     public void setConnectionString(String connectionString) {
